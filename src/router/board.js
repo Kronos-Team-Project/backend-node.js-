b95f7d8f9ce00e4ece3126a38cbf70e5b8e70554
kimgyuha <kimgyuha123@naver.com>
2022-09-07 13:52:03 +0900

    [add] boards.js router

---
+++ src/router/board.js
@@ -0,0 +1,10 @@
+const router = require('express')();
+const controller = require('../controllers/boards');
+
+router.post('/write', controller.write);
+router.put('/update', controller.update);
+router.delete('/delete', controller.deletecontent);
+router.get('/get', controller.readOneBoard);
+router.get('/star', controller.readStar);
+
+module.exports = router;
