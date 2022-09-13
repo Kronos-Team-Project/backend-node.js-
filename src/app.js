4340f38149ef65a99052b3989f5560652f7c1a5d
kimgyuha <kimgyuha25@gmail.com>
2022-09-13 21:54:27 +0900

    [update] app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const bodyPraser = require('body-parser');
-const cors = require('cors');
-const { sequelize } = require('./models');
+const express = require("express");
+const bodyPraser = require("body-parser");
+const cors = require("cors");
+const { sequelize } = require("./models");
 
-const router = require('./router/index');
+const router = require("./router/index");
 
 const app = express();
 
@@ -11,25 +11,31 @@ const PORT = process.env.PORT || 3030;
 
 app.use(cors());
 
-app.use(bodyPraser.json());
-app.use(bodyPraser.urlencoded({extended : true}));
-app.use('/', router);
+const corsOptions = {
+  origin: "*",
+  method: ["GET", "POST", "PATCH", "DELETE"],
+  credentials: true,
+};
 
-app.get('/', (req, res)=> {
-    res.json({message : `Server is running on port ${PORT}`});
+app.use(cors(corsOptions));
+
+app.use(bodyPraser.json());
+app.use(bodyPraser.urlencoded({ extended: true }));
+app.use("/", router);
+
+app.get("/", (req, res) => {
+  res.json({ message: `Server is running on port ${PORT}` });
 });
 
-sequelize.sync({force : false}) //연결 유무 확인
-    .then(() =>{
-        console.log("database 연결 성공");
-    })
-    .catch((err) =>{
-        console.log(err);
-    });
-
+sequelize
+  .sync({ force: false }) //연결 유무 확인
+  .then(() => {
+    console.log("database 연결 성공");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
-
